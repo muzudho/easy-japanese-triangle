f1921a3ffbd5d813d57c521d975e1aff39285577
むずでょ <muzudho1@gmail.com>
2021-01-10 15:26:16 +0900

    WIP. file_line

--- main.py
+++ main.py
@@ -25,7 +25,40 @@ class Piece(Enum):
     BOTH = 3
 
 
-def starting_sq(index, n):
+class Direction(Enum):
+    """
+    . ->
+    """
+    RIGHT = 0
+    """
+    .
+    |
+    v
+    """
+    DOWN = 1
+
+
+def starting_x(e, n):
+    """Example:
+
+    n = 4
+
+    ...a
+    ..b.
+    .c..
+    d...
+
+    f(e) = sq
+    f(1) =  3 ... a
+    f(2) =  2 ... b
+    f(3) =  1 ... c
+    f(4) =  0 ... d
+    """
+    # `e` is 1 origin.
+    return n-(e-1)
+
+
+def sq_of_e(e, n):
     """Starting square.
     Example:
 
@@ -36,12 +69,38 @@ def starting_sq(index, n):
     .c..
     d...
 
-    f(0) = 3 ... a
-    f(1) = 6 ... b
-    f(2) = 9 ... c
-    f(3) = 12 ... d
+    f(e) = sq
+    f(1) =  3 ... a
+    f(2) =  6 ... b
+    f(3) =  9 ... c
+    f(4) = 12 ... d
     """
-    return (index+1) * (n-1)
+
+    # `e` is 1 origin.
+    # `sq` is 0 origin.
+    return e * (n-1)
+
+
+def get_step_and_end_sq(dir, e, n):
+    """Ending square.
+    Example:
+
+    n = 4
+
+    ..c.
+    ....
+    b.a.
+    ....
+
+    b = int(a / n)
+    c = a mod n
+
+    """
+    sq = sq_of_e(e, n)
+    if dir == Direction.RIGHT.value:
+        return -1, int(sq/n)
+    else:
+        return -n, sq % n
 
 
 def random_dir():
@@ -73,20 +132,43 @@ def print_board(board, n):
         for column in range(0, n):
             print(f"{board[row*n+column]} ", end="")
         print("")  # New line.
-    pass
+
+
+def fill_line(board, dir, e, n):
+    # x = starting_x(e, n)
+    sq = sq_of_e(e, n)
+    # print(f"e={e} sq_of_e={sq} starting_x={x}")
+    print(f"e={e} sq_of_e={sq} dir={dir}")
+    step, end_sq = get_step_and_end_sq(dir, e, n)
+    print(f"step={step} end_sq={end_sq}")
+    while sq != end_sq:
+        if dir == Piece.RIGHT.value:
+            board[sq] = Piece.RIGHT.value
+        else:
+            board[sq] = Piece.DOWN.value
+        sq += step
 
 
 print("start")
 
 # Elemental number
 n = 3
+print(f"n={n}")
 
 board = create_board(n)
+print_board(board, n)
 
-for index in range(0, n):
-    print(f"n={n} index={index} stargin sq={starting_sq(index,n)}")
+print(f"Top row:")
+fill_line(board, Direction.RIGHT.value, 1, n)
+print_board(board, n)
+
+print(f"Leftest column:")
+fill_line(board, Direction.DOWN.value, n, n)
+print_board(board, n)
+
+for e in range(0, n):
     dir = random_dir()
-    print(f"dir={dir}")
+    fill_line(board, dir, e, n)
     print_board(board, n)
     pass
 
